1efca8def932c39068c24cba36ac37005aeca5b8
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2026-01-12 09:42:13 -0700

    cool

--- lists.py
+++ lists.py
@@ -1,1 +1,27 @@
-#
+# GNB - 1st - Lists Types Notes
+
+shawties = ["Emmie", "Sophie", "Elijah", "Annie", "Emily", "Isabel"]
+
+print(shawties[0])
+shawties[-1] = "Jordan" #changes thing in that vertex
+
+
+print(shawties)
+
+#Tuple
+
+fave_food = ("Jollof Rice", "Platanos Fritos", "Fried Chicken", "Fries", "Granola")
+
+fave_food[1] = "Mango"
+
+#Set
+
+countries = {"Nigeria", "Spain", "Columbia", "France", "England", "Mali", "Samoa", "Tonga"}
+
+countries.add("Uzbekistan")
+countries.remove("England")
+
+for country in countries:
+    print(country)
+    if country == "France":
+        print("Bounjour, madame")
