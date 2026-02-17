471f91cd20beafccfad5444d4a0f41b0bb862b29
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2026-02-17 09:52:33 -0700

    I very, very strongly dislike this

---
+++ project_structure_notes/src (project name)/helper.py
@@ -0,0 +1,2 @@
+# Functions that are used all over the place in your code such as input santization
+# Don't import any other files into this file
---
+++ project_structure_notes/src (project name)/main.py
@@ -0,0 +1,7 @@
+# All Python files go in the src folder
+
+# main.py is required ; This is where the project runs from
+# Almost nothing is on thie page, it's only meant to call other pages with functions that run the program
+# Basic UI is allowed on this page, but it's better to have it on a seperate file
+
+# Only import onto main if at all possible ; Don't import multiple files into your other pages
