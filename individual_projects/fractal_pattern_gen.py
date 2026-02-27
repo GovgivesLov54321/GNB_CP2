51d35168311991ca1d78102e491d0b68a702ac0f
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2026-02-27 09:52:21 -0700

    something else ig

--- individual_projects/fractal_pattern_gen.py
+++ individual_projects/fractal_pattern_gen.py
@@ -1,2 +1,20 @@
 # GNB - 1st - Fractal Pattern Generator (what're we doing with these assignments LaRose? 💔)
+#import turtle cause it'll prob help
 import turtle as t
+
+# Welcome user osns: "Welcome to the Sierpinski Triangle Generator! This program creates a Sierpinski Triangle fractal using recursion."
+
+
+
+# define function triangle_turtle(color, depth)
+    # get our turtle the shape of a turtle
+    # color will be given when this function called by main osns (as well as depth)
+
+
+
+
+
+# define main function or wtv... 
+    # Have user choose a recursion depth (1-5); should have an int(eger) thing
+    # Have user choose a color between these: (red, green, blue, yellow, orange, purple, cyan, magenta, black, white)
+    # After cool triangle generation, give user option of enter to exit program, or draw another triangle (if I can figure that out)
