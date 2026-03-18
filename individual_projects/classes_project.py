bed84327c988c158525ecd939315423eb701b953
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2026-03-18 09:52:50 -0600

    cool ig

---
+++ individual_projects/classes_project.py
@@ -0,0 +1,14 @@
+# GNB - 1st - Classes Project Option 2: Geometry Calculator
+
+class Circle:
+    def __init__(self,circle,radius, diameter, area, perimeter, ):
+        self.circle = circle
+        self.radius
+
+
+class Rectangle:
+    class Square:
+        pass
+
+class Triangle:
+    pass
