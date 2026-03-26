4cf09bc36d926c7cfad4cab312871f9d1e63585e
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2026-03-26 09:52:41 -0600

    mi bombo fahh ah project osns

---
+++ individual_projects/classes_relationship_project/grade_book.py
@@ -0,0 +1,24 @@
+# GNB - 1st - Class Relationships Project Option 1: Simple Grade Book
+#Lwk kinda understand what needs to be done
+import math
+
+
+students = []
+
+class Student:
+    def __init__(self, name, student_id, grade_list):
+        self.name = name
+        self.student_id = student_id
+        self.grade_list = grade_list
+
+    def add_student(student_name, student_id, class_and_grade):
+        student_name = input("What is the name of the student you'd like to add? (First & Last):").strip()
+        student_id = int(input("What's their four-digit student ID?: "))
+        check_number_of_classes= int(input("How many classes does this student have? (Enter number):"))
+        for 
+    
+
+
+class GradeBook:
+    def __init__(self):
+        pass
---
+++ individual_projects/classes_relationship_project/main.py
@@ -0,0 +1,2 @@
+# GNB - 1st - main thing for grade book frfr
+from grade_book import *
