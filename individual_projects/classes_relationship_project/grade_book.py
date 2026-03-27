12ae9ec0c6865b6713902100683b21ba2503bf73
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2026-03-27 09:52:42 -0600

    mi bombo fahh ah project osns

--- individual_projects/classes_relationship_project/grade_book.py
+++ individual_projects/classes_relationship_project/grade_book.py
@@ -1,9 +1,8 @@
 # GNB - 1st - Class Relationships Project Option 1: Simple Grade Book
 #Lwk kinda understand what needs to be done
 import math
+import csv
 
-
-students = []
 
 class Student:
     def __init__(self, name, student_id, grade_list):
@@ -11,12 +10,17 @@ class Student:
         self.student_id = student_id
         self.grade_list = grade_list
 
-    def add_student(student_name, student_id, class_and_grade):
-        student_name = input("What is the name of the student you'd like to add? (First & Last):").strip()
+    def add_student(student_name, student_id):
+        student_name = input("What is the name of the student you'd like to add? (Format = First Middle initial Last): ").strip()
         student_id = int(input("What's their four-digit student ID?: "))
-        check_number_of_classes= int(input("How many classes does this student have? (Enter number):"))
-        for 
     
+    def find_student():
+        search_choice = input("Which would you like to search by (Enter a number):" \
+        "1. Student Last Name" \
+        "2. Student ID Number")
+        if search_choice == 1:
+            with open("individual_projects\classes_relationship_project\grade_list.csv", mode = "r+") as csv_file:
+
 
 
 class GradeBook:
