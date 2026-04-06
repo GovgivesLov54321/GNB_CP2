06725a7221914e24e26e95d2243cafe91bf56589
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2026-04-06 09:50:35 -0600

    tried to do notes ig

---
+++ notes/gui.py
@@ -0,0 +1,28 @@
+# GNB - 1st - GUI Notes
+import tkinter as tk
+
+root = tk.Tk()
+
+
+root.title("Testing")
+root.configure(background = "orange")
+root.minsize = (250, 250)
+root.maxsize = (670, 670)
+root.geometry("300x300+100+100")
+#everything we want should be inbetween these two commands
+label = tk.Label(root, text = "This is currently working :)", font = ("", 67, "bold"))
+label.config(fg = "turquoise", background = "orange")
+label.pack()
+image = tk.PhotoImage(file = "notes\pngtree-fried-chicken-png-png-image_14516402.png")
+tk.Label(root, image = image).pack()
+
+#Stuff about button
+root.count = 0
+def add():
+    root.count += 1
+    tk.Label(root, text = root.count).pack()
+
+
+btn = tk.Button(root, text = "ADD", command = add)
+btn.pack()
+root.mainloop()
