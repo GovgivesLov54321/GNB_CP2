4dca29d3067a652fd2a991003292e4648a674859
GovgivesLov54321 <governor.bakare@ucas-edu.net>
2026-04-07 09:09:26 -0600

    done fr

--- notes/gui.py
+++ notes/gui.py
@@ -13,16 +13,21 @@ root.geometry("300x300+100+100")
 label = tk.Label(root, text = "This is currently working :)", font = ("", 67, "bold"))
 label.config(fg = "turquoise", background = "orange")
 label.pack()
-image = tk.PhotoImage(file = "notes\pngtree-fried-chicken-png-png-image_14516402.png")
-tk.Label(root, image = image).pack()
+#image = tk.PhotoImage(file = "notes\pngtree-fried-chicken-png-png-image_14516402.png")
+#tk.Label(root, image = image).pack()
 
 #Stuff about button
 root.count = 0
 def add():
     root.count += 1
-    tk.Label(root, text = root.count).pack()
+    num["text"] = root.count
+  #  tk.Label(root, text = root.count).pack()
 
 
 btn = tk.Button(root, text = "ADD", command = add)
 btn.pack()
+num = tk.Label(root, text = "0")
+num.pack()
+label.pack()
+
 root.mainloop()
